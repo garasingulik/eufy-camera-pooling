936102b3d469c6a9d85a576ab47dca6525218ada
Indra Gunawan <feedsbrain@gmail.com>
2020-10-26 16:18:42 +0800

    Set Timeout for Stream Checking

--- src/lib/ffprobe.ts
+++ src/lib/ffprobe.ts
@@ -3,12 +3,12 @@ import * as childProcess from 'child_process'
 import config from '../config'
 import * as T from './types'
 
-const TEN_MEGA_BYTE = 1024 * 1024 * 10
-
 export const getStreamInfo = (url: string): T.R<string> => {
   return new Promise((resolve, reject) => {
-    const cmd = `${config.ffprobe} -v quiet -print_format json -show_format -show_streams "${url}"`
-    childProcess.exec(cmd, { maxBuffer: TEN_MEGA_BYTE }, (error, stdout, stderr) => {
+    const cmd = `${config.ffprobe} -v quiet -print_format json -show_format -show_streams -rtsp_flags prefer_tcp "${url}"`
+    childProcess.exec(cmd, {
+      timeout: 5000
+    }, (error, stdout, stderr) => {
       if (error) {
         return reject(error)
       }
